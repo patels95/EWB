3dbbef1b785c9e23e8e8f9cca71c269030429a5e
Sanam Patel <patels326@gmail.com>
2015-08-12 19:30:06 -0400

    added projects activity

--- app/src/main/java/com/patels95/sanam/ewb/adapters/ProjectAdapter.java
+++ app/src/main/java/com/patels95/sanam/ewb/adapters/ProjectAdapter.java
@@ -1,6 +1,7 @@
 package com.patels95.sanam.ewb.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,10 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.patels95.sanam.ewb.R;
 import com.patels95.sanam.ewb.model.Project;
+import com.patels95.sanam.ewb.ui.ProjectsActivity;
 
 
 public class ProjectAdapter extends RecyclerView.Adapter<ProjectAdapter.ProjectViewHolder> {
@@ -71,7 +72,9 @@ public class ProjectAdapter extends RecyclerView.Adapter<ProjectAdapter.ProjectV
 
         @Override
         public void onClick(View v) {
-            Toast.makeText(mContext, mTitle.getText().toString(), Toast.LENGTH_LONG).show();
+//            Toast.makeText(mContext, mTitle.getText().toString(), Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(mContext, ProjectsActivity.class);
+            mContext.startActivity(intent);
         }
     }
 
