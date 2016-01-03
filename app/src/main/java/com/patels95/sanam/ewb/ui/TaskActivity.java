9c888cd57408226834abbfd7ea2c8d58316ed4db
patels95 <patels326@gmail.com>
2016-01-02 21:26:44 -0500

    delete task alert dialog

--- app/src/main/java/com/patels95/sanam/ewb/ui/TaskActivity.java
+++ app/src/main/java/com/patels95/sanam/ewb/ui/TaskActivity.java
@@ -1,18 +1,82 @@
 package com.patels95.sanam.ewb.ui;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.parse.ParseUser;
 import com.patels95.sanam.ewb.R;
+import com.patels95.sanam.ewb.model.ParseConstants;
 
 public class TaskActivity extends ActionBarActivity {
+
+    private DialogInterface.OnClickListener mDialogListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which) {
+                case DialogInterface.BUTTON_POSITIVE:
+                    // delete the task
+                    break;
+                case DialogInterface.BUTTON_NEGATIVE:
+                    // close dialog
+                    dialog.dismiss();
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        if (ParseUser.getCurrentUser() != null) {
+            getMenuInflater().inflate(R.menu.menu_task, menu);
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_logout:
+                ParseUser.logOut();
+                navigateToMain();
+                break;
+            case R.id.action_delete_task:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("Are you sure that you want to delete this task?");
+                builder.setPositiveButton("Yes", mDialogListener);
+                builder.setNegativeButton("Cancel", mDialogListener);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // start main activity after logout
+    private void navigateToMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        Toast.makeText(this, "You have been logged out.", Toast.LENGTH_LONG).show();
     }
 
 }
