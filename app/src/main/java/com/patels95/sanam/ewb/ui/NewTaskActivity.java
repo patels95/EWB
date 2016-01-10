6e759dc0c3520e03eaa993e134e23fb5545f400b
patels95 <patels326@gmail.com>
2016-01-10 18:47:18 -0500

    new task activity layout added EditTexts and save button

--- app/src/main/java/com/patels95/sanam/ewb/ui/NewTaskActivity.java
+++ app/src/main/java/com/patels95/sanam/ewb/ui/NewTaskActivity.java
@@ -2,10 +2,17 @@ package com.patels95.sanam.ewb.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.EditText;
 
 import com.patels95.sanam.ewb.R;
 
+import butterknife.InjectView;
+
 public class NewTaskActivity extends ActionBarActivity {
+
+    @InjectView(R.id.new_task_title) EditText mTaskTitle;
+    @InjectView(R.id.new_task_description) EditText mTaskDescription;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
