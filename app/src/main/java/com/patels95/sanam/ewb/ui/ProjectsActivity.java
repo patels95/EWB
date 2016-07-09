1d28b915392b53191ba4cd37eb10fcab5746fdbb
patels95 <patels326@gmail.com>
2016-07-09 11:51:14 -0400

    added tab selector styles

--- app/src/main/java/com/patels95/sanam/ewb/ui/ProjectsActivity.java
+++ app/src/main/java/com/patels95/sanam/ewb/ui/ProjectsActivity.java
@@ -78,6 +78,23 @@ public class ProjectsActivity extends ActionBarActivity implements ActionBar.Tab
         mProjectTabs.addTab(tasks, 0);
         mProjectTabs.addTab(resources, 1);
 
+        mProjectTabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                mProjectPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
         mProjectPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mProjectTabs));
 
 
